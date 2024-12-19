e50e37ad4321d8905dc4aefdfb5cfddb3c9f0d90
aryansamaria <aryansamaria44@gmail.com>
2024-12-19 19:28:25 +0530

    jmp_newz

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,37 @@
+class ApplicationController < ActionController::Base
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+  protect_from_forgery with: :exception
+
+  def login(user)
+    session[:user_id] = user.id 
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logout
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  def logged_in?
+    current_user.nil? ? false : true
+  end
+
+  helper_method :current_user, :logged_in?
+
+  def prevent_unauthorized_user_access
+    unless logged_in?
+      redirect_to root_path, notice: 'sorry, you cannot access that page', status: :found
+    end 
+  end
+
+  def prevent_logged_in_user_access
+    if logged_in?
+      redirect_to root_path, notice: 'You are already logged in.', status: :found
+    end
+  end
+
+end
---
+++ app/models/link.rb
@@ -0,0 +1,30 @@
+class Link < ApplicationRecord
+  belongs_to :user
+  has_many :comments
+  has_many :votes
+  validates :title,
+            presence: true,
+            uniqueness: { case_sensitive: false }
+            
+  validates :url,
+          format: { with: %r{\Ahttps?://} },
+          allow_blank: true
+
+  def comment_count
+    comments.length
+  end
+    
+  def upvotes
+    votes.sum(:upvote)
+  end
+  
+  def increment_vote
+    increment!(:votes)
+  end
+  
+  def downvotes
+    votes.sum(:downvote)
+  end
+
+       
+end
